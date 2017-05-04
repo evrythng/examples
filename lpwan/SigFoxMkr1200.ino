46f15c7f53ef378b3a5cb3d25a2ab8d6438e16da
Joël Vogt <joelvogt@users.noreply.github.com>
2017-05-04 17:50:41 +0100

    Changed sleep duration

--- lpwan/SigFoxMkr1200.ino
+++ lpwan/SigFoxMkr1200.ino
@@ -13,36 +13,47 @@ void sendTemperature() {
   // Start the module
   SigFox.begin();
   // Wait at least 30mS after first configuration (100mS before)
-  delay(100);
+  delay(1000);
   // Clears all pending interrupts
   
   SigFox.status();
-  delay(1);
+  delay(1000);
   SigFox.beginPacket();
   uint8_t v= SigFox.internalTemperature();
   SigFox.write(v);
 
-  int ret = SigFox.endPacket(true);  // send buffer to SIGFOX network
+  int ret = SigFox.endPacket();  // send buffer to SIGFOX network
   SigFox.end();
 }
 
 void setup() {
+
   // Uncomment this line and comment begin() if you are working with a custom board
+  //if (!SigFox.begin(SPI1, 30, 31, 33, 28, LED_BUILTIN)) {
   if (!SigFox.begin()) {
     return;
   }
   // Enable debug led and disable automatic deep sleep
   // Comment this line when shipping your project :)
-  SigFox.debug();
+SigFox.debug();
+
   delay(100);
+
   // Send the module to the deepest sleep
   SigFox.end();
+
 }
 
 
 void loop() {
   // Every 15 minutes, read all the sensors and send them
-  sendTemperature();
+  // Let's try to optimize the data format
+  // Only use floats as intermediate representaion, don't send them directly
+sendTemperature();
+
+
+
   //Sleep for 15 minutes
-  LowPower.sleep(15 * 60 * 1000);
+  LowPower.sleep(20 * 60 * 1000);
 }
+
