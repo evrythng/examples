00ffa19b79cbe3a338158e007434f8f0b96e49ae
James Every <james.every@gmail.com>
2018-03-27 11:47:15 +0100

    ActiveMQ with persistent queues PoC (#8)
    * ActiveMQ with persistent queues PoC
    
    * Ignore local queues

--- customer-solutions/supply-chain/evrythng.eif/src/main/java/com/evrythng/demo/supplychain/EVTLoader.java
+++ customer-solutions/supply-chain/evrythng.eif/src/main/java/com/evrythng/demo/supplychain/EVTLoader.java
@@ -12,12 +12,16 @@ import java.net.URL;
 public abstract class EVTLoader {
 
     public static ApiManager newApiManager() {
-        try {
-            URL url = new URL(System.getenv("EVT_URL"));
+            URL url = readEVTUrl();
             String EVT_KEY = System.getenv("EVT_KEY");
             ApiConfiguration apiConfiguration = new ApiConfiguration(EVT_KEY);
             apiConfiguration.setUrl(url.toString());
             return new ApiManager(apiConfiguration);
+    }
+
+    private static URL readEVTUrl() {
+        try {
+            return new URL(System.getenv("EVT_URL"));
         } catch (MalformedURLException e) {
             throw new RuntimeException("Malformed URL in ENV variable EVT_URL", e);
         }
