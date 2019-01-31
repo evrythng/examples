0dd2874ac6572472e25e5e74f7ec50c8b5122820
C-D-Lewis <Bonsitm@gmail.com>
2019-01-31 10:58:37 +0000

    Update MQTT example

--- pubsub/MQTT/mqttNode.js
+++ pubsub/MQTT/mqttNode.js
@@ -1,36 +1,25 @@
-var mqtt = require('mqtt');
+const mqtt = require('mqtt');
 
-var thngId='UWFd8YMe8VKwfYDxUhbUkrNh'
-var thngUrl='/thngs/'+thngId
+// Export these in your shell session (MQTT_URL has a default value)
+const {
+  DEVICE_API_KEY,
+  THNG_ID,
+  MQTT_URL = 'mqtts://mqtt.evrythng.com:8883',
+} = process.env;
 
-// This is using the unsecure version - for simplicity's sake - obviously, don't use it in production
-var client = mqtt.connect("mqtt://mqtt.evrythng.com:1883", {
-  username: 'authorization',
-  password: '' //Your device API KEY
-});
+const main = () => {
+  // Create client and authorize with broker
+  const client = mqtt.connect(MQTT_URL, { username: 'authorization', password: DEVICE_API_KEY });
 
-client.on('connect', function () { // Called once when connected
-  client.subscribe(thngUrl+'/properties/');
-  client.subscribe(thngUrl+'/actions/');
-  setTimeout(updateProperty, 2000); // We set a callback in 2 seconds
-});
+  // When connected, subscribe to Thng property updates
+  client.on('connect', () => {
+    console.log('Connected!');
 
-// This will be called every time a property is updated or an event changes
-client.on('message', function (topic, message) {
-  console.log(message.toString());
-});
+    client.subscribe(`/thngs/${THNG_ID}/properties`);
+  });
 
-// update a property
-function updateProperty () {
-  // Let's set the light in a random time span
-  var level = Math.round(Math.random()*1000);
-  client.publish(thngUrl+'/properties/lightlevel', '[{"value": '+level+'}]');
-  setTimeout(updateProperty, 5000); // We set a callback in 2 seconds
-}
+  // When a message is received, print it
+  client.on('message', (topic, message) => console.log(`${topic}: ${message}`));
+};
 
-
-// Let's close this connection cleanly
-process.on('SIGINT', function() {
-  client.end();
-  process.exit();
-});
+main();
